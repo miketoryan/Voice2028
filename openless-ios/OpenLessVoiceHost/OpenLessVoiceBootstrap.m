4fc5222fab5e02552a2ffc996fe928a6008526eb
miketoryan <ryan.only@outlook.com>
2026-09-25 10:14:05 +0800

    Add OpenLess GPT voice bootstrap constructor

--- openless-ios/OpenLessVoiceHost/OpenLessVoiceBootstrap.m
+++ openless-ios/OpenLessVoiceHost/OpenLessVoiceBootstrap.m
@@ -1,18 +1,27 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
-#import <objc/message.h>
-
-static void OpenLessStartVoiceRuntime(void) {
-    Class cls = NSClassFromString(@"OpenLessVoiceRuntime");
-    SEL selector = NSSelectorFromString(@"startShared");
-    if (cls && [cls respondsToSelector:selector]) {
-        ((void (*)(id, SEL))objc_msgSend)(cls, selector);
-    }
-}
 
 __attribute__((constructor))
-static void OpenLessVoiceBootstrap(void) {
+static void OpenLessVoiceBootstrapConstructor(void) {
     dispatch_async(dispatch_get_main_queue(), ^{
-        OpenLessStartVoiceRuntime();
+        Class cls = NSClassFromString(@"OpenLessVoiceBootstrap");
+        if (!cls) return;
+
+        id shared = nil;
+        SEL sharedSelector = NSSelectorFromString(@"shared");
+        if ([cls respondsToSelector:sharedSelector]) {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+            shared = [cls performSelector:sharedSelector];
+#pragma clang diagnostic pop
+        }
+
+        SEL startSelector = NSSelectorFromString(@"start");
+        if (shared && [shared respondsToSelector:startSelector]) {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+            [shared performSelector:startSelector];
+#pragma clang diagnostic pop
+        }
     });
 }
